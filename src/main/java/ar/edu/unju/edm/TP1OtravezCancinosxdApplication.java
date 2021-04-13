52495d2832030401b74c1fbc89c63723a635839f
Mario <Mario@Pool-PC>
2021-04-13 04:26:32 -0300

    Segunda Parte del TP1 Queda a resolver el problema de impresion de pantalla a la hora de un resultado

---
+++ src/main/java/ar/edu/unju/edm/TP1OtravezCancinosxdApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TP1OtravezCancinosxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TP1OtravezCancinosxdApplication.class, args);
+	}
+
+}
